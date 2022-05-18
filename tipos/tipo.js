e9fafdc82bbf27dec37ef0937a9c04e64d5f354c
Carlos Rocha <carlosandrade399@gmail.com>
2022-05-18 14:04:15 -0300

    Tipos Numéricos e Booleano

--- tipos/tipo.js
+++ tipos/tipo.js
@@ -3,3 +3,13 @@
 let nome = "João";
 console.log(nome);
 // nome = 34 (gera erro, pois o tipo é string)
+// tipo number
+let idade = 29;
+//idade = 'ana' (gera erro pois o tipo é number)
+idade = 34.8;
+console.log(idade);
+// tipos number podem receber inteiro e reais (ponto flutuante)
+// tipo boolean
+let possuiHobbies = true;
+console.log(possuiHobbies);
+// possuiHobbies = 1 (gera um erro por o tipo é boolen)
--- tipos/tipo.ts
+++ tipos/tipo.ts
@@ -1,5 +1,17 @@
 // tipo string
 let nome = "João";
 console.log(nome);
-
 // nome = 34 (gera erro, pois o tipo é string)
+
+// tipo number
+
+let idade = 29;
+//idade = 'ana' (gera erro pois o tipo é number)
+idade = 34.8;
+console.log(idade);
+// tipos number podem receber inteiro e reais (ponto flutuante)
+
+// tipo boolean
+let possuiHobbies = true;
+console.log(possuiHobbies);
+// possuiHobbies = 1 (gera um erro por o tipo é boolen)
