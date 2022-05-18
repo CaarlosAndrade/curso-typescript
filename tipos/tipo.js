f4ea6c0285c742dbb4098c265ed36a2cdcd0558c
Carlos Rocha <carlosandrade399@gmail.com>
2022-05-18 13:52:44 -0300

    Tipo String

---
+++ tipos/tipo.js
@@ -0,0 +1,5 @@
+"use strict";
+// tipo string
+let nome = "João";
+console.log(nome);
+// nome = 34 (gera erro, pois o tipo é string)
---
+++ tipos/tipo.ts
@@ -0,0 +1,5 @@
+// tipo string
+let nome = "João";
+console.log(nome);
+
+// nome = 34 (gera erro, pois o tipo é string)
